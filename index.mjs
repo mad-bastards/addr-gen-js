3a128af1c5bb03be5e2a387b67ad6bfadced94fd
nobody <github2@invisiblehand.church>
2024-01-16 07:46:47 -0500

    x

--- index.mjs
+++ index.mjs
@@ -1,27 +1,39 @@
 import coininfo from 'coininfo';
 import CoinKey from 'coinkey';
 import fs from 'fs';
-const keys=Object.keys;
-const coins = keys(coininfo);
-console.dump=function(obj) {
-  this.log(JSON.stringify(obj,null,2));
-};
-const hex_key=
-  "038109007313a5807b2eccc082c8c3fbb988a973cacf1a7df9ce725c31b14776";
-const key = Buffer.from(hex_key,'hex');
-var ci;
-const vector={};
-vector.key=key.toString("hex");
-vector.pub=new CoinKey(key).publicKey.toString('hex');
-vector.addrs={};
-for( const coin of coins ) {
-  const vec=vector.addrs[coin]={};
-  const ci = coininfo(coin);
-  console.dump(ci);
-  vec.ver=ci.versions.public;
-  const ck = new CoinKey(key,ci);
-  vec.addr=ck.publicAddress;
-  vec.sym=ck.unit;
-};
+var coins=["decred"];
+coins = Object.keys(coininfo);
+coins.sort();
+//   console.dump=function(obj) {
+//     this.log(JSON.stringify(obj,null,2));
+//   };
+const keys = [
+  "038109007313a5807b2eccc082c8c3fbb988a973cacf1a7df9ce725c31b14776",
+  "0000000000000000000000000000000000000000000000000000000000000001"
+];
+const vector=[];
+for(const hex_key of keys ) {
+  const key = Buffer.from(hex_key,'hex');
+  var ci;
+  const set={};
+  vector.push(set);
+  set.key=key.toString("hex");
+  set.pub=new CoinKey(key).publicKey.toString('hex');
+  set.addrs={};
+  for( const coin of coins ) {
+    const vec={ coin };
+    const ci = coininfo(coin);
+    vec.pubver=ci.versions.public;
+    vec.priver=ci.versions.private;
+    const ck = new CoinKey(key,ci);
+    console.log(ck);
+    vec.addr=ck.publicAddress;
+    vec.sym=ci.unit;
+    vec.wif=ck.privateWif;
+    set.addrs[vec.sym]=vec;
+  }
+}
 
-fs.writeFileSync('vector.json',JSON.stringify(vector,0,2));
+const text=JSON.stringify(vector,0,2);
+console.log(text);
+fs.writeFileSync('vector.json',text);
