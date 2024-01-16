a9d95bc6ea101bcf85c350e067caa213ac45fc73
nobody <github2@invisiblehand.church>
2024-01-16 15:54:46 -0500

    removed usage of new Buffer, for Buffer.from

--- index.mjs
+++ index.mjs
@@ -11,29 +11,35 @@ const keys = [
   "038109007313a5807b2eccc082c8c3fbb988a973cacf1a7df9ce725c31b14776",
   "0000000000000000000000000000000000000000000000000000000000000001"
 ];
-const vector=[];
+const vector={};
+vector.coins={};
+vector.addrs=[];
+for( const coin of coins ) {
+  const vec={ coin };
+  const ci = coininfo(coin);
+  vec.pubver=ci.versions.public;
+  vec.priver=ci.versions.private;
+  vec.sym=ci.unit.toLowerCase();
+  vector.coins[vec.sym]=vec;
+}
 for(const hex_key of keys ) {
   const key = Buffer.from(hex_key,'hex');
   var ci;
   const set={};
-  vector.push(set);
+  vector.addrs.push(set);
   set.key=key.toString("hex");
   set.pub=new CoinKey(key).publicKey.toString('hex');
   set.addrs={};
   for( const coin of coins ) {
-    const vec={ coin };
+    const vec={ };
     const ci = coininfo(coin);
-    vec.pubver=ci.versions.public;
-    vec.priver=ci.versions.private;
+    vec.sym=ci.unit.toLowerCase();
     const ck = new CoinKey(key,ci);
-    console.log(ck);
     vec.addr=ck.publicAddress;
-    vec.sym=ci.unit;
     vec.wif=ck.privateWif;
     set.addrs[vec.sym]=vec;
   }
 }
 
 const text=JSON.stringify(vector,0,2);
-console.log(text);
 fs.writeFileSync('vector.json',text);
